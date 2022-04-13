fdd80a28506f1e07967b49197b2b21ebb2cba7aa
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-13 11:37:02 -0700

    initial setup files

---
+++ macros/create_tasks.sql
@@ -0,0 +1,7 @@
+{% macro create_tasks() %}
+    {% if target.database == 'OSMOSIS' %}
+        CREATE SCHEMA IF NOT EXISTS _internal;
+        {{ task_run_sp_create_prod_clone('_internal') }};
+    {% endif %}
+
+{% endmacro %}
