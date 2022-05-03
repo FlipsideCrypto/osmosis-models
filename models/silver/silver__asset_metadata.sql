750a754c5a9a49d068b0237c9e219d3b8c0c098e
Desmond Hui <desmond@flipsidecrypto.com>
2022-05-03 11:29:03 -0700

    dedupe asset metadata before putting into silver

--- models/silver/silver__asset_metadata.sql
+++ models/silver/silver__asset_metadata.sql
@@ -2,16 +2,32 @@
   materialized = 'table'
 ) }}
 
-SELECT 
-    'osmosis' AS blockchain, 
-    base AS address, 
-    'flipside' AS creator, 
-    'token' AS label_type, 
-    'token_contract' AS label_subtype, 
-    name AS label, 
-    symbol AS project_name,  
-    denom_units AS value
-FROM {{ source(
-        'osmosis_external', 
-        'asset_metadata_api'
+WITH base AS (
+
+  SELECT
+    base AS address,
+    NAME AS label,
+    symbol AS project_name,
+    denom_units AS VALUE
+  FROM
+    {{ source(
+      'osmosis_external',
+      'asset_metadata_api'
     ) }}
+  GROUP BY
+    1,
+    2,
+    3,
+    4
+)
+SELECT
+  'osmosis' AS blockchain,
+  address,
+  'flipside' AS creator,
+  'token' AS label_type,
+  'token_contract' AS label_subtype,
+  label,
+  project_name,
+  VALUE
+FROM
+  base
