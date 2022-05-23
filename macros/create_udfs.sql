945e2e488a469d1ea4b3c77960e8d83a5072693d
Desmond Hui <desmond@flipsidecrypto.com>
2022-05-23 16:21:39 -0700

    integrate balances api, add raw balances external table

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,7 +1,10 @@
 {% macro create_udfs() %}
     {% set sql %}
     {{ udf_bulk_get_asset_metadata() }};
-{# Add crate udf macros here #}
+    {% endset %}
+    {% do run_query(sql) %}
+    {% set sql %}
+    {{ udf_bulk_get_balances() }};
     {% endset %}
     {% do run_query(sql) %}
 {% endmacro %}
---
+++ models/silver/silver__all_unknown_balances.sql
@@ -0,0 +1,58 @@
+{{ config(
+    materialized = 'view',
+    post_hook = 'call silver.sp_bulk_get_balances()',
+) }}
+
+WITH all_wallets AS (
+
+    SELECT
+        DISTINCT attribute_value AS address
+    FROM
+        osmosis_dev.silver.msg_attributes
+    WHERE
+        RLIKE(
+            attribute_value,
+            'osmo\\w{39}'
+        )
+        AND block_timestamp :: DATE <= '2022-05-26' -- some snapshot date
+),
+wallets_per_block AS (
+    SELECT
+        DISTINCT block_id,
+        attribute_value AS address
+    FROM
+        osmosis_dev.silver.msg_attributes
+    WHERE
+        RLIKE(
+            attribute_value,
+            'osmo\\w{39}'
+        )
+        AND block_id > 2383300
+),
+possible_balances_needed AS (
+    SELECT
+        2383300 AS block_id,
+        address
+    FROM
+        all_wallets
+    UNION ALL
+    SELECT
+        *
+    FROM
+        wallets_per_block
+)
+SELECT
+    block_id,
+    address
+FROM
+    possible_balances_needed
+EXCEPT
+SELECT
+    DISTINCT block_id,
+    address
+FROM
+    {{ source('osmosis_external','balances_api') }}
+ORDER BY
+    block_id
+LIMIT
+    50000
