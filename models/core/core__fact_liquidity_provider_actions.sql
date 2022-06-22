3cec22351c1a977e79b6e31dd5743c0bd7060d0a
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-06-22 11:08:08 -0400

    gold view

--- models/core/core__fact_liquidity_provider_actions.sql
+++ models/core/core__fact_liquidity_provider_actions.sql
@@ -3,16 +3,33 @@
 ) }}
 
 SELECT
-    block_id, 
-    block_timestamp, 
-    blockchain, 
-    chain_id, 
-    tx_id, 
-    tx_status, 
-    liquidity_provider_address, 
-    action, 
-    pool_id, 
-    amount, 
-    currency, 
-    decimal
-FROM {{ ref('silver__liquidity_provider_actions') }}
+    block_id,
+    block_timestamp,
+    blockchain,
+    chain_id,
+    tx_id,
+    tx_status,
+    liquidity_provider_address,
+    action,
+    pool_id,
+    amount,
+    currency,
+    DECIMAL
+FROM
+    {{ ref('silver__liquidity_provider_actions') }}
+UNION ALL
+SELECT
+    block_id,
+    block_timestamp,
+    blockchain,
+    chain_id,
+    tx_id,
+    tx_status,
+    liquidity_provider_address,
+    action,
+    pool_id,
+    amount,
+    currency,
+    DECIMAL
+FROM
+    {{ ref('silver__early_liquidity_provider_actions') }}
