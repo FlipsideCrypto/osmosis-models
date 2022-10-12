40b325ec75dab4400ed51feb39f902b80b730b21
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-10-12 10:19:14 -0400

    better way to check for prod env

--- macros/streamline/bulk_get_balances/udf_bulk_get_balances.sql
+++ macros/streamline/bulk_get_balances/udf_bulk_get_balances.sql
@@ -1,6 +1,6 @@
 {% macro udf_bulk_get_balances() %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION silver.udf_bulk_get_balances() returns text api_integration = aws_osmosis_api_dev AS {% if target.name == "prod" -%}
+    OR REPLACE EXTERNAL FUNCTION silver.udf_bulk_get_balances() returns text api_integration = aws_osmosis_api_dev AS {% if target.database == 'OSMOSIS' %}
         'https://k7jc1bnb8i.execute-api.us-east-1.amazonaws.com/prod/bulk_get_balances'
     {% else %}
         'https://auacbjh2tj.execute-api.us-east-1.amazonaws.com/dev/bulk_get_balances'
