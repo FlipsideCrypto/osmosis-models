00cc62af39f264d4f50eeb879dd954e135b50567
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-11-09 11:48:53 -0500

    tx count macro and silver table

---
+++ macros/snowflake_api/get_blockchain_api.sql
@@ -0,0 +1,96 @@
+{% macro get_blockchain_api() %}
+  {% set query %}
+  CREATE TABLE if NOT EXISTS bronze.blockchain_api(
+    block_ids ARRAY,
+    call STRING,
+    DATA variant,
+    _inserted_timestamp timestamp_ntz
+  );
+{% endset %}
+  {% do run_query(query) %}
+  {% set query %}
+INSERT INTO
+  bronze.blockchain_api(
+    block_ids,
+    call,
+    DATA,
+    _inserted_timestamp
+  ) WITH base AS (
+    SELECT
+      MIN(block_id) min_block,
+      MAX(block_id) max_block,
+      ARRAY_AGG(block_id) blocks
+    FROM
+      (
+        SELECT
+          conditional_true_event(
+            CASE
+              WHEN rn_mod = 1 THEN TRUE
+              ELSE FALSE
+            END
+          ) over (
+            ORDER BY
+              block_ID
+          ) groupID,
+          block_id
+        FROM
+          (
+            SELECT
+              block_Id,
+              MOD(ROW_NUMBER() over(
+            ORDER BY
+              block_id), 20) rn_mod
+            FROM
+              (
+                SELECT
+                  DISTINCT block_id
+                FROM
+                  bronze.blocks
+                WHERE
+                  block_id > 6300426
+                EXCEPT
+                SELECT
+                  VALUE
+                FROM
+                  bronze.blockchain_api,
+                  LATERAL FLATTEN(block_ids)
+              )
+            ORDER BY
+              1
+          )
+      )
+    GROUP BY
+      groupID
+    LIMIT
+      20
+  )
+SELECT
+  blocks,
+  CONCAT(
+    'https://osmosis-coke.allthatnode.com:56657/blockchain?minHeight=',
+    min_block,
+    '&maxHeight=',
+    max_block
+  ) AS call,
+  ethereum.streamline.udf_api(
+    'GET',
+    CONCAT(
+      'https://osmosis-coke.allthatnode.com:56657/blockchain?minHeight=',
+      min_block,
+      '&maxHeight=',
+      max_block
+    ),{ 'Authorization':(
+      SELECT
+        key
+      FROM
+        osmosis._internal.api_keys
+      WHERE
+        provider = 'allthatnode'
+    ) },{}
+  ) AS resp,
+  SYSDATE()
+FROM
+  base;
+{% endset %}
+  {% do run_query(query) %}
+{% endmacro %}
