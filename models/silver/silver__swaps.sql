00dd5f56ece4c0da180ac2ab3da198491214c47d
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-11-17 21:12:31 +0000

    fix a rama (#73)

--- models/silver/silver__swaps.sql
+++ models/silver/silver__swaps.sql
@@ -81,37 +81,6 @@ pre_final AS (
         b.value :routes IS NOT NULL
         AND b.index = rn
 ),
-max_idx AS (
-    SELECT
-        tx_id,
-        _body_index,
-        MAX(
-            r.index
-        ) AS max_index
-    FROM
-        pre_final p,
-        TABLE(FLATTEN(routes)) r
-    GROUP BY
-        tx_id, 
-        _body_index
-),
-to_cur AS (
-    SELECT
-        p.tx_id,
-        p._body_index,
-        COALESCE(
-            r.value :tokenOutDenom :: STRING,
-            r.value :token_out_denom :: STRING
-        ) AS to_currency
-    FROM
-        pre_final p
-        INNER JOIN max_idx m
-        ON p.tx_id = m.tx_id
-        AND p._body_index = m._body_index
-        LEFT OUTER JOIN TABLE(FLATTEN(routes)) r
-    WHERE
-        r.index = max_index
-),
 pools AS (
     SELECT
         tx_id,
@@ -121,28 +90,27 @@ pools AS (
         ) AS pool_ids
     FROM
         pre_final p,
-    TABLE(FLATTEN(routes)) r
+        TABLE(FLATTEN(routes)) r
     GROUP BY
         tx_id,
-        _body_index
-),
-msg_idx AS (
-    SELECT
-        p.tx_id,
-        msg_group,
-        MIN(
-            m.msg_index
-        ) AS min_msg_index
-    FROM
-        pre_final p
-        INNER JOIN {{ ref('silver__msg_attributes') }}
-        m
-        ON p.tx_id = m.tx_id
-    WHERE
-        (
-            msg_type = 'token_swapped'
-            AND attribute_key = 'tokens_in'
-        )
+        _body_index),
+        msg_idx AS (
+            SELECT
+                p.tx_id,
+                msg_group,
+                MIN(
+                    m.msg_index
+                ) AS min_msg_index
+            FROM
+                pre_final p
+                INNER JOIN {{ ref('silver__msg_attributes') }}
+                m
+                ON p.tx_id = m.tx_id
+            WHERE
+                (
+                    msg_type = 'token_swapped'
+                    AND attribute_key = 'tokens_in'
+                )
 
 {% if is_incremental() %}
 AND m._inserted_timestamp >= (
@@ -208,6 +176,64 @@ OR (
     AND attribute_key = 'amount'
 )
 ),
+max_idx2 AS (
+    SELECT
+        p.tx_id,
+        msg_group,
+        MAX(
+            m.msg_index
+        ) AS max_msg_index
+    FROM
+        pre_final p
+        INNER JOIN osmosis_dev.silver.msg_attributes m
+        ON p.tx_id = m.tx_id
+    WHERE
+        (
+            msg_type = 'token_swapped'
+            AND attribute_key = 'tokens_out'
+        )
+        OR (
+            msg_type = 'transfer'
+            AND attribute_key = 'amount'
+        )
+        AND msg_group IS NOT NULL
+    GROUP BY
+        p.tx_id,
+        msg_group
+),
+to_amt AS (
+    SELECT
+        mm.tx_id,
+        p.msg_index,
+        mm.msg_group,
+        RIGHT(attribute_value, LENGTH(attribute_value) - LENGTH(SPLIT_PART(TRIM(REGEXP_REPLACE(attribute_value, '[^[:digit:]]', ' ')), ' ', 0))) AS to_currency,
+        SPLIT_PART(
+            TRIM(
+                REGEXP_REPLACE(
+                    attribute_value,
+                    '[^[:digit:]]',
+                    ' '
+                )
+            ),
+            ' ',
+            0
+        ) AS to_amount
+    FROM
+        osmosis_dev.silver.msg_attributes p
+        INNER JOIN max_idx2 mm
+        ON p.tx_id = mm.tx_id
+        AND p.msg_group = mm.msg_group
+        AND p.msg_index = max_msg_index
+    WHERE
+        (
+            msg_type = 'token_swapped'
+            AND attribute_key = 'tokens_out'
+        )
+        OR (
+            msg_type = 'transfer'
+            AND attribute_key = 'amount'
+        )
+),
 pre_final2 AS (
     SELECT
         block_id,
@@ -223,10 +249,10 @@ pre_final2 AS (
             WHEN f.from_currency LIKE 'gamm/pool/%' THEN 18
             ELSE l.raw_metadata [1] :exponent
         END AS from_decimal,
-        to_amount,
-        to_currency,
+        tt.to_amount,
+        tt.to_currency,
         CASE
-            WHEN t.to_currency LIKE 'gamm/pool/%' THEN 18
+            WHEN tt.to_currency LIKE 'gamm/pool/%' THEN 18
             ELSE A.raw_metadata [1] :exponent
         END AS TO_DECIMAL,
         pool_ids,
@@ -240,11 +266,11 @@ pre_final2 AS (
         LEFT OUTER JOIN from_amt f
         ON p.tx_id = f.tx_id
         AND p._body_index = f.msg_group
-        LEFT OUTER JOIN to_cur t
-        ON p.tx_id = t.tx_id
-        AND p._body_index = t._body_index
+        LEFT OUTER JOIN to_amt tt
+        ON p.tx_id = tt.tx_id
+        AND p._body_index = tt.msg_group
         LEFT OUTER JOIN {{ ref('silver__asset_metadata') }} A
-        ON t.to_currency = A.address
+        ON tt.to_currency = A.address
         LEFT OUTER JOIN {{ ref('silver__asset_metadata') }}
         l
         ON f.from_currency = l.address
