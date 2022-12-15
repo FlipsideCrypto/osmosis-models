e2cf67598ab36fff5d51455eda1821b030b67953
jhuhnke <jessica@flipsidecrypto.com>
2022-12-15 08:10:17 -0600

    remove reference to deprecated bronze table

--- models/core/core__dim_prices.sql
+++ models/core/core__dim_prices.sql
@@ -2,23 +2,32 @@
     materialized = 'view'
 ) }}
 
-SELECT 
-    recorded_at, 
-    symbol, 
-    price, 
-    total_supply, 
-    volume_24h, 
-    provider
-FROM {{ ref('silver__prices') }}
-
-UNION ALL 
-
-SELECT 
-    block_hour AS recorded_at, 
-    project_name AS symbol, 
-    price_usd AS price, 
-    NULL AS total_supply, 
-    NULL AS volume_24h, 
-    'swaps' AS provider 
-
-FROM {{ ref('silver__prices_swaps') }}
+SELECT
+    recorded_hour AS recorded_at,
+    symbol,
+    CLOSE AS price,
+    NULL AS total_supply,
+    NULL AS volume_24h,
+    'coin gecko' AS provider
+FROM
+    {{ ref('silver__token_prices_coin_gecko_hourly') }}
+UNION ALL
+SELECT
+    recorded_hour AS recorded_at,
+    symbol,
+    CLOSE AS price,
+    NULL AS total_supply,
+    NULL AS volume_24h,
+    'coin market cap' AS provider
+FROM
+    {{ ref('silver__token_prices_coin_market_cap_hourly') }}
+UNION ALL
+SELECT
+    block_hour AS recorded_at,
+    project_name AS symbol,
+    price_usd AS price,
+    NULL AS total_supply,
+    NULL AS volume_24h,
+    'swaps' AS provider
+FROM
+    {{ ref('silver__prices_swaps') }}
---
+++ models/silver/prices/silver__token_prices_coin_market_cap_hourly.sql
@@ -0,0 +1,274 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "_unique_key",
+    incremental_strategy = 'merge',
+    cluster_by = ['recorded_hour::DATE'],
+) }}
+
+WITH date_hours AS (
+
+    SELECT
+        date_hour
+    FROM
+        {{ source (
+            'crosschain',
+            'dim_date_hours'
+        ) }}
+    WHERE
+        date_hour >= '2020-04-10'
+        AND date_hour <= (
+            SELECT
+                MAX(recorded_hour)
+            FROM
+                {{ source(
+                    'crosschain_silver',
+                    'hourly_prices_coin_market_cap'
+                ) }}
+        )
+
+{% if is_incremental() %}
+AND date_hour > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+asset_metadata AS (
+    SELECT
+        id,
+        symbol
+    FROM
+        {{ source(
+            'crosschain_silver',
+            'asset_metadata_coin_market_cap'
+        ) }}
+    WHERE
+        id IN (
+            7431,
+            7281,
+            3874,
+            3635,
+            7271,
+            12008,
+            4679,
+            3794,
+            14973,
+            17748,
+            2909,
+            16214,
+            8905,
+            13314,
+            19899,
+            18051,
+            8279,
+            17338,
+            3773,
+            4315,
+            5604,
+            9908,
+            8996,
+            5835,
+            2620,
+            4263,
+            17208,
+            9480,
+            3717,
+            2396,
+            12220,
+            7226,
+            17799,
+            4846, 
+            14299
+        )
+    GROUP BY
+        1,
+        2
+),
+base_date_hours_symbols AS (
+    SELECT
+        date_hour,
+        id,
+        symbol
+    FROM
+        date_hours
+        CROSS JOIN asset_metadata
+),
+base_legacy_prices AS (
+    SELECT
+        DATE_TRUNC(
+            'hour',
+            recorded_at
+        ) AS recorded_hour,
+        asset_id :: NUMBER AS id,
+        symbol,
+        price AS CLOSE
+    FROM
+        {{ source(
+            'shared',
+            'prices_v2'
+        ) }}
+    WHERE
+        provider = 'coinmarketcap'
+        AND asset_id IN (
+            7431,
+            7281,
+            3874,
+            3635,
+            7271,
+            12008,
+            4679,
+            3794,
+            14973,
+            17748,
+            2909,
+            16214,
+            8905,
+            13314,
+            19899,
+            18051,
+            8279,
+            17338,
+            3773,
+            4315,
+            5604,
+            9908,
+            8996,
+            5835,
+            2620,
+            4263,
+            17208,
+            9480,
+            3717,
+            2396,
+            12220,
+            7226,
+            17799,
+            4846, 
+            14299
+        )
+        AND MINUTE(recorded_at) = 59
+        AND recorded_at :: DATE < '2022-07-20' -- use legacy data before this date
+
+{% if is_incremental() %}
+AND recorded_at > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+base_prices AS (
+    SELECT
+        recorded_hour,
+        p.id,
+        m.symbol,
+        p.close
+    FROM
+        {{ source(
+            'crosschain_silver',
+            'hourly_prices_coin_market_cap'
+        ) }}
+        p
+        LEFT OUTER JOIN asset_metadata m
+        ON m.id = p.id
+    WHERE
+        p.id IN (
+            7431,
+            7281,
+            3874,
+            3635,
+            7271,
+            12008,
+            4679,
+            3794,
+            14973,
+            17748,
+            2909,
+            16214,
+            8905,
+            13314,
+            19899,
+            18051,
+            8279,
+            17338,
+            3773,
+            4315,
+            5604,
+            9908,
+            8996,
+            5835,
+            2620,
+            4263,
+            17208,
+            9480,
+            3717,
+            2396,
+            12220,
+            7226,
+            17799,
+            4846, 
+            14299
+        )
+        AND recorded_hour :: DATE >= '2022-07-20'
+
+{% if is_incremental() %}
+AND recorded_hour > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+prices AS (
+    SELECT
+        *
+    FROM
+        base_legacy_prices
+    UNION
+    SELECT
+        *
+    FROM
+        base_prices
+),
+imputed_prices AS (
+    SELECT
+        d.*,
+        p.close AS hourly_close,
+        LAST_VALUE(
+            p.close ignore nulls
+        ) over (
+            PARTITION BY d.symbol
+            ORDER BY
+                d.date_hour rows unbounded preceding
+        ) AS imputed_close
+    FROM
+        base_date_hours_symbols d
+        LEFT OUTER JOIN prices p
+        ON p.recorded_hour = d.date_hour
+        AND p.id = d.id
+)
+SELECT
+    p.date_hour AS recorded_hour,
+    p.id,
+    p.symbol,
+    COALESCE(
+        p.hourly_close,
+        p.imputed_close
+    ) AS CLOSE,
+    CASE
+        WHEN p.hourly_close IS NULL THEN TRUE
+        ELSE FALSE
+    END AS imputed,
+    concat_ws(
+        '-',
+        recorded_hour,
+        id
+    ) AS _unique_key
+FROM
+    imputed_prices p
+WHERE
+    CLOSE IS NOT NULL
