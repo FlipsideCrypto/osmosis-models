51c9582878c434c9087ce445df5a70a88a5e1960
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2023-01-05 14:43:07 +0000

    remove reference to deprecated bronze table (#84)
    * remove reference to deprecated bronze table
    
    * silver prices
    
    * update rpc URL to be dynamic
    
    * add ez prices
    
    Co-authored-by: Eric Laurello <eric.laurello@flipsidecrypto.com>

---
+++ models/core/core__ez_prices.sql
@@ -0,0 +1,38 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+WITH p_base AS (
+
+    SELECT
+        DATE_TRUNC(
+            'hour',
+            recorded_at
+        ) AS recorded_hour,
+        UPPER(
+            A.symbol
+        ) AS symbol,
+        A.price,
+        A.provider,
+        CASE
+            A.provider
+            WHEN 'coin gecko' THEN 1
+            WHEN 'coin market cap' THEN 2
+            ELSE 3
+        END AS pro_rank
+    FROM
+        {{ ref('core__dim_prices') }} A qualify(ROW_NUMBER() over(PARTITION BY recorded_hour, UPPER(symbol)
+    ORDER BY
+        pro_rank) = 1))
+    SELECT
+        A.recorded_hour,
+        A.symbol,
+        b.address AS currency,
+        A.price
+    FROM
+        p_base A
+        LEFT JOIN {{ ref('silver__asset_metadata') }}
+        b
+        ON A.symbol = UPPER (
+            b.project_name
+        )
