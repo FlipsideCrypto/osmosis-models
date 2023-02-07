32159205d629980e854bdfe79dde2e1c960efa19
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-02-07 12:49:01 -0500

    gold views

--- models/silver/silver__daily_balances.sql
+++ models/silver/silver__daily_balances.sql
@@ -301,3 +301,78 @@ SELECT
     ) AS balance
 FROM
     balance_temp
+UNION ALL
+SELECT
+    A.block_timestamp :: DATE AS DATE,
+    'pool' AS balance_type,
+    A.pool_address AS address,
+    token_0_denom currency,
+    DECIMAL,
+    token_0_amount balance
+FROM
+    {{ ref('silver__pool_balances') }} A
+    LEFT JOIN {{ ref('silver__asset_metadata') }}
+    am
+    ON A.token_0_denom = am.address
+
+{% if is_incremental() %}
+WHERE
+    A.block_timestamp :: DATE >= (
+        SELECT
+            DATEADD('day', -1, MAX(DATE))
+        FROM
+            {{ this }})
+        {% endif %}
+
+        qualify(ROW_NUMBER() over(PARTITION BY block_timestamp :: DATE, pool_id, token_0_denom
+        ORDER BY
+            block_timestamp DESC) = 1)
+        UNION ALL
+        SELECT
+            A.block_timestamp :: DATE AS DATE,
+            'pool' AS balance_type,
+            A.pool_address AS address,
+            token_1_denom currency,
+            DECIMAL,
+            token_1_amount balance
+        FROM
+            {{ ref('silver__pool_balances') }} A
+            LEFT JOIN {{ ref('silver__asset_metadata') }}
+            am
+            ON A.token_1_denom = am.address qualify(ROW_NUMBER() over(PARTITION BY block_timestamp :: DATE, pool_id, token_1_denom
+        ORDER BY
+            block_timestamp DESC) = 1)
+        UNION ALL
+        SELECT
+            A.block_timestamp :: DATE AS DATE,
+            'pool' AS balance_type,
+            A.pool_address AS address,
+            token_2_denom currency,
+            DECIMAL,
+            token_2_amount balance
+        FROM
+            {{ ref('silver__pool_balances') }} A
+            LEFT JOIN {{ ref('silver__asset_metadata') }}
+            am
+            ON A.token_2_denom = am.address
+        WHERE
+            token_2_denom IS NOT NULL qualify(ROW_NUMBER() over(PARTITION BY block_timestamp :: DATE, pool_id, token_2_denom
+        ORDER BY
+            block_timestamp DESC) = 1)
+        UNION ALL
+        SELECT
+            A.block_timestamp :: DATE AS DATE,
+            'pool' AS balance_type,
+            A.pool_address AS address,
+            token_3_denom currency,
+            DECIMAL,
+            token_3_amount balance
+        FROM
+            {{ ref('silver__pool_balances') }} A
+            LEFT JOIN {{ ref('silver__asset_metadata') }}
+            am
+            ON A.token_3_denom = am.address
+        WHERE
+            token_3_denom IS NOT NULL qualify(ROW_NUMBER() over(PARTITION BY block_timestamp :: DATE, pool_id, token_3_denom
+        ORDER BY
+            block_timestamp DESC) = 1)
