b81ddff3f0df5dbbcf633c0eb2dfd376209bad94
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-02-14 15:37:02 -0500

    validator commission

---
+++ models/core/core__fact_validator_commission.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'view',
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'STAKING' }}}
+) }}
+
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    tx_succeeded,
+    tx_caller_address,
+    msg_group,
+    amount,
+    currency,
+    validator_address_operator,
+    validator_address_reward
+FROM
+    {{ ref('silver__validator_commission') }}
