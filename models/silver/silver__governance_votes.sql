2978408e0014f7d6a7ec3afd5cc52f845dda14b3
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2023-02-20 10:45:13 -0600

    adds in memo field to votes table (#103)
    * adds in memo field to votes table
    
    * efficiency
    
    ---------
    
    Co-authored-by: Eric Laurello <eric.laurello@flipsidecrypto.com>

--- models/silver/silver__governance_votes.sql
+++ models/silver/silver__governance_votes.sql
@@ -5,126 +5,143 @@
     cluster_by = ['block_timestamp::DATE'],
 ) }}
 
-WITH
-
-{% if is_incremental() %}
-max_date AS (
+WITH base_tx AS (
 
     SELECT
-        MAX(
-            _inserted_timestamp
-        ) _inserted_timestamp
+        block_id,
+        block_timestamp,
+        tx_id,
+        tx_succeeded,
+        tx_body :memo :: STRING AS memo,
+        tx_body :messages AS messages,
+        _inserted_timestamp
     FROM
-        {{ this }}
-),
-{% endif %}
+        {{ ref('silver__transactions') }}
 
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            )
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+memo_text AS (
+    SELECT
+        tx_id,
+        memo
+    FROM
+        base_tx
+    WHERE
+        memo IS NOT NULL
+),
 weighted_votes AS (
     SELECT
         block_id,
         block_timestamp,
         tx_id,
         tx_succeeded,
-        tx_body,
+        messages,
         path :: STRING AS _path,
         _inserted_timestamp
     FROM
-        {{ ref('silver__transactions') }},
+        base_tx,
         LATERAL FLATTEN (
-            input => tx_body :messages,
+            input => messages,
             recursive => TRUE
         ) b
     WHERE
         key = '@type'
         AND VALUE :: STRING = '/cosmos.gov.v1beta1.MsgVoteWeighted'
-
-{% if is_incremental() %}
-AND _inserted_timestamp >= (
+),
+pre_final AS (
     SELECT
-        MAX(
-            _inserted_timestamp
-        )
-    FROM
-        max_date
-)
-{% endif %}
-)
-SELECT
-    block_id,
-    block_timestamp,
-    tx_id,
-    tx_succeeded,
-    path :: STRING AS _path,
-    this :proposal_id :: STRING AS proposal_id,
-    this :voter :: STRING AS voter,
-    CASE
-        WHEN this :option :: STRING = 'VOTE_OPTION_YES' THEN 1
-        WHEN this :option :: STRING = 'VOTE_OPTION_ABSTAIN' THEN 2
-        WHEN this :option :: STRING = 'VOTE_OPTION_NO' THEN 3
-        WHEN this :option :: STRING = 'VOTE_OPTION_NO_WITH_VETO' THEN 4
-        ELSE this :option
-    END AS vote_option,
-    1.000 AS vote_weight,
-    _inserted_timestamp,
-    concat_ws(
-        '-',
+        block_id,
+        block_timestamp,
         tx_id,
-        proposal_id,
-        voter,
-        vote_option,
-        _path
-    ) AS _unique_key
-FROM
-    {{ ref('silver__transactions') }},
-    LATERAL FLATTEN (
-        input => tx_body :messages,
-        recursive => TRUE
-    ) b
-WHERE
-    key = '@type'
-    AND VALUE :: STRING = '/cosmos.gov.v1beta1.MsgVote'
-
-{% if is_incremental() %}
-AND _inserted_timestamp >= (
-    SELECT
-        MAX(
-            _inserted_timestamp
-        )
+        tx_succeeded,
+        path :: STRING AS _path,
+        this :proposal_id :: STRING AS proposal_id,
+        this :voter :: STRING AS voter,
+        CASE
+            WHEN this :option :: STRING = 'VOTE_OPTION_YES' THEN 1
+            WHEN this :option :: STRING = 'VOTE_OPTION_ABSTAIN' THEN 2
+            WHEN this :option :: STRING = 'VOTE_OPTION_NO' THEN 3
+            WHEN this :option :: STRING = 'VOTE_OPTION_NO_WITH_VETO' THEN 4
+            ELSE this :option
+        END AS vote_option,
+        1.000 AS vote_weight,
+        _inserted_timestamp,
+        concat_ws(
+            '-',
+            tx_id,
+            proposal_id,
+            voter,
+            vote_option,
+            _path
+        ) AS _unique_key
     FROM
-        max_date
+        base_tx,
+        LATERAL FLATTEN (
+            input => messages,
+            recursive => TRUE
+        ) b
+    WHERE
+        key = '@type'
+        AND VALUE :: STRING = '/cosmos.gov.v1beta1.MsgVote'
+    UNION ALL
+    SELECT
+        block_id,
+        block_timestamp,
+        tx_id,
+        tx_succeeded,
+        _path,
+        b.value :proposal_id :: NUMBER AS proposal_id,
+        b.value :voter :: STRING AS voter,
+        CASE
+            WHEN o.value :option :: STRING = 'VOTE_OPTION_YES' THEN 1
+            WHEN o.value :option :: STRING = 'VOTE_OPTION_ABSTAIN' THEN 2
+            WHEN o.value :option :: STRING = 'VOTE_OPTION_NO' THEN 3
+            WHEN o.value :option :: STRING = 'VOTE_OPTION_NO_WITH_VETO' THEN 4
+            ELSE o.value :option
+        END AS vote_option,
+        o.value :weight :: FLOAT AS vote_weight,
+        _inserted_timestamp,
+        concat_ws(
+            '-',
+            tx_id,
+            proposal_id,
+            voter,
+            vote_option,
+            _path
+        ) AS _unique_key
+    FROM
+        weighted_votes,
+        LATERAL FLATTEN (
+            input => messages
+        ) b,
+        LATERAL FLATTEN (
+            input => b.value :options
+        ) o
 )
-{% endif %}
-UNION ALL
 SELECT
     block_id,
     block_timestamp,
-    tx_id,
+    m.tx_id,
     tx_succeeded,
     _path,
-    b.value :proposal_id :: NUMBER AS proposal_id,
-    b.value :voter :: STRING AS voter,
-    CASE
-        WHEN o.value :option :: STRING = 'VOTE_OPTION_YES' THEN 1
-        WHEN o.value :option :: STRING = 'VOTE_OPTION_ABSTAIN' THEN 2
-        WHEN o.value :option :: STRING = 'VOTE_OPTION_NO' THEN 3
-        WHEN o.value :option :: STRING = 'VOTE_OPTION_NO_WITH_VETO' THEN 4
-        ELSE o.value :option
-    END AS vote_option,
-    o.value :weight :: FLOAT AS vote_weight,
+    proposal_id,
+    voter,
+    vote_option,
+    vote_weight,
+    memo,
     _inserted_timestamp,
-    concat_ws(
-        '-',
-        tx_id,
-        proposal_id,
-        voter,
-        vote_option,
-        _path
-    ) AS _unique_key
+    _unique_key
 FROM
-    weighted_votes,
-    LATERAL FLATTEN (
-        input => tx_body :messages
-    ) b,
-    LATERAL FLATTEN (
-        input => b.value :options
-    ) o
+    pre_final p
+    LEFT OUTER JOIN memo_text m
+    ON p.tx_id = m.tx_id
