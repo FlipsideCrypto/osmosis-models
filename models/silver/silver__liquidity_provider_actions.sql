67dca171e1df01cbde1e298818ac25f336e5c1b9
shah <info@shahnewazkhan.ca>
2023-05-12 16:06:04 -0700

    added cluster_by keys for reference models from streamline queries

--- models/silver/silver__liquidity_provider_actions.sql
+++ models/silver/silver__liquidity_provider_actions.sql
@@ -2,7 +2,7 @@
     materialized = 'incremental',
     unique_key = "_unique_key",
     incremental_strategy = 'merge',
-    cluster_by = ['block_timestamp::DATE'],
+    cluster_by = ['block_timestamp::DATE','block_id::NUMBER'],
 ) }}
 
 WITH
--- models/silver/silver__msg_attributes.sql
+++ models/silver/silver__msg_attributes.sql
@@ -2,7 +2,7 @@
   materialized = 'incremental',
   unique_key = "_unique_key",
   incremental_strategy = 'merge',
-  cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
+  cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE','block_id::NUMBER'],
   post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
