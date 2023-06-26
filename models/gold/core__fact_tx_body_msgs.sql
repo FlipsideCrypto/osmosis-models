53b827cdf6df1dfb256266516e74e4474c9a75de
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2023-06-26 13:42:13 -0400

    add tx body model to gold (#129)

---
+++ models/gold/core__fact_tx_body_msgs.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    tx_succeeded,
+    msg_group AS msg_index,
+    msg_type,
+    msg
+FROM
+    {{ ref('silver__tx_body_msgs') }}
