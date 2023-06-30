c4137bb4cad4d3611a5a3d3328670903f2a110ac
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-06-30 09:08:30 -0400

    block gaps

---
+++ models/silver/_observability/silver_observability__block_gaps.sql
@@ -0,0 +1,72 @@
+{{ config(
+    materialized = 'incremental',
+    full_refresh = false
+) }}
+
+WITH source AS (
+
+    SELECT
+        block_id,
+        block_timestamp,
+        LAG(
+            block_id,
+            1
+        ) over (
+            ORDER BY
+                block_id ASC
+        ) AS prev_BLOCK_ID
+    FROM
+        {{ ref('silver__blocks') }}
+        {# WHERE
+        block_timestamp < DATEADD(HOUR, -15, getdate()) #}
+
+{% if is_incremental() %}
+WHERE
+    (
+        block_timestamp :: DATE >= (
+            SELECT
+                MAX(
+                    _inserted_timestamp
+                ) :: DATE -3
+            FROM
+                {{ this }}
+        )
+        OR (
+            (
+                SELECT
+                    block_gaps
+                FROM
+                    {{ this }}
+                    qualify(ROW_NUMBER() over(
+                ORDER BY
+                    _inserted_timestamp DESC) = 1)
+            ) <> 0
+        )
+    )
+{% endif %}
+)
+SELECT
+    MIN(block_id) AS min_block_id,
+    MAX(block_id) AS max_block_id,
+    MIN(block_timestamp) AS min_block_timestamp,
+    MAX(block_timestamp) AS max_block_timestamp,
+    COUNT(1) AS total_blocks,
+    SUM(
+        CASE
+            WHEN block_id - prev_BLOCK_ID <> 1 THEN 1
+            ELSE 0
+        END
+    ) AS block_gaps,
+    ARRAY_AGG(
+        CASE
+            WHEN block_id - prev_BLOCK_ID <> 1 THEN OBJECT_CONSTRUCT(
+                'prev_block_id',
+                prev_block_id,
+                'block_id',
+                block_id
+            )
+        END
+    ) AS block_gaps_details,
+    SYSDATE() AS _inserted_timestamp
+FROM
+    source
