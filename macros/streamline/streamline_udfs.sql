fec4213460c04e7f913884e1a2a9f00b0ce8fef3
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-05-21 13:39:33 -0400

    missing url

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -3,7 +3,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_rest_api_v2(
         json OBJECT
     ) returns ARRAY api_integration = {% if target.name == "prod" %}
-        aws_osmosis_api_prod AS ''
+        aws_osmosis_api_prod AS 'https://9ybmmw2jk7.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_rest_api'
     {% else %}
         aws_osmosis_api_stg AS 'https://sz3utstqtb.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api'
     {%- endif %};
