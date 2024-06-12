9aceda7e9e83c7d76ed6ebd75ef690df295e42f0
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-06-12 17:49:51 -0400

    batching

--- models/streamline/silver/core/realtime/streamline__tx_counts_realtime.sql
+++ models/streamline/silver/core/realtime/streamline__tx_counts_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"txcount_v2",
-        "sql_limit" :"100000",
+        "sql_limit" :"15000",
         "producer_batch_size" :"300",
         "worker_batch_size" :"100",
         "sql_source" :"{{this.identifier}}" }
