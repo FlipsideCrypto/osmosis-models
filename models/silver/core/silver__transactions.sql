9fd1051aa5b8bac29d9fcf0cb4321c4d4083984a
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-08-09 10:35:37 -0400

    cluster key

--- models/silver/core/silver__transactions.sql
+++ models/silver/core/silver__transactions.sql
@@ -4,7 +4,7 @@
   incremental_strategy = 'merge',
   incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
   merge_exclude_columns = ["inserted_timestamp"],
-  cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
+  cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE','modified_timestamp::DATE'],
   post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_id)",
   tags = ['core']
 ) }}
