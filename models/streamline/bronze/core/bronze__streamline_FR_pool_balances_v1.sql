f5fe6525b7c5e26f0a80ddca7f39867c174ce69f
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-05-01 09:53:46 -0400

    fix FR

--- models/streamline/bronze/core/bronze__streamline_FR_pool_balances_v1.sql
+++ models/streamline/bronze/core/bronze__streamline_FR_pool_balances_v1.sql
@@ -3,7 +3,7 @@
     tags = ['core']
 ) }}
 {{ streamline_external_table_FR_query(
-    model = 'balances',
+    model = 'pool_balances',
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
     partition_name = "_partition_by_block_id",
     unique_key = 'block_number'
